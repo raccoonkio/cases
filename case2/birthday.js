452831f94620e0d06c6dd9d3c5ca8535cc997f55
raccoonkio <91130812+raccoonkio@users.noreply.github.com>
2025-07-13 17:19:12 +0300

    Update birthday.js

--- case2/birthday.js
+++ case2/birthday.js
@@ -1,3 +1,31 @@
+
+    const day = parseInt(document.getElementById("day").value);
+    const month = parseInt(document.getElementById("month").value) - 1;
+    const year = parseInt(document.getElementById("year").value);
+    dateBirth = new Date (year, month, day);
+    febriaryInBirthYear = new Date (year, 2, 0);
+
 function checkDateBirth(){
-    alert('run');
+    //const day = parseInt(document.getElementById("day").value);
+    //const month = parseInt(document.getElementById("month").value) + 1;
+    //const year = parseInt(document.getElementById("year").value);
+    const week = ["Воскресенье", "Понедельник", "Вторник", "Среда", "Четверг", "Пятница", "Суббота"];
+    //console.log(dateBirth);
+
+    //let dateBirth = new Date (year, month, day);
+    let weekDay = dateBirth.getDay();
+
+    document.getElementById("weekDay").innerHTML = week[weekDay];
+
+}
+function checkLeapYear(){
+    daysInFebriary = febriaryInBirthYear.getDate();
+    if (daysInFebriary > 28) {
+        document.getElementById("lear-year").innerHTML = year + "год был високостным";
+    }
+}
+
+function runCheckDateBirth(){
+    checkDateBirth();
+    checkLeapYear();
 }
