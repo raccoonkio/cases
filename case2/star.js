72af69580985d30721baf1a0ac2569e667457fe0
raccoonkio <91130812+raccoonkio@users.noreply.github.com>
2025-07-26 07:23:54 +0300

    Create star.js

---
+++ case2/star.js
@@ -0,0 +1,13 @@
+
+
+
+/*let result = '';
+for (let i = 0; i < 5; i++){
+    let resultStr = '';
+    for (let j = 0; j < 10; j++){
+    resultStr = resultStr + numStars[j][i] + ' ' ;
+    }
+    result = result + resultStr + '\r\n';
+}
+console.log(result);
+*/
